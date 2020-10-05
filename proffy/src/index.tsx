6a0a815a1b0fec5251f918c8dab139a35c04d20a
AmandaDeCarli <amandadecarli@outlook.com>
2020-10-05 19:17:22 -0300

    Projeto Proffy

---
+++ proffy/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Chama o arquivo App.tsx que contém os
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
